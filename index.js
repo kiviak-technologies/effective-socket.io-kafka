45e0e1a842923c541edec09a1c549a9db3f0c70f
Clément LEDOUX <clm@MacBook-Pro-de-Clement.local>
2020-01-31 11:06:06 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+
+/**
+ * Dependencies
+ */
+
+const Adapter = require('socket.io-adapter');
+
+/**
+ * Exports
+ */
+
+module.exports = Kafka;
+
+/**
+ * Kafka adapter class
+ */
+
+class Kafka extends Adapter {
+
+  constructor(nsp) {
+    super(nsp);
+  }
+
+
+  
+}
